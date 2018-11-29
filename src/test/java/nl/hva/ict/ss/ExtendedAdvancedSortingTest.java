ea6cff24091b722ff3a1ba66a34001adf7da7c2b
Rowan Visser <rowan.visser@hotmail.com>
2018-11-29 17:33:16 +0100

    Added tests

--- src/test/java/nl/hva/ict/ss/ExtendedAdvancedSortingTest.java
+++ src/test/java/nl/hva/ict/ss/ExtendedAdvancedSortingTest.java
@@ -1,8 +1,7 @@
 package nl.hva.ict.ss;
 
 import java.util.LinkedList;
-import java.util.concurrent.TimeUnit;
-import static nl.hva.ict.ss.AdvancedSortingTest.UPPER_LIMIT;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -11,11 +10,11 @@ import org.junit.Test;
  * here will result in failing the assignment!
  */
 public class ExtendedAdvancedSortingTest extends AdvancedSortingTest {
-
+int numberOfPlayers = 100;
     @Test
     public void measureEfficiencyArray() {
         System.out.printf("Array implementation V2%n");
-        int numberOfPlayers = 20;
+       
         Player[] players = getSubList(unsortedPlayers, numberOfPlayers).toArray(new Player[numberOfPlayers]);
 
         Player[] playersSorted = AdvancedSorts.quickSort(players);
@@ -28,7 +27,7 @@ public class ExtendedAdvancedSortingTest extends AdvancedSortingTest {
     @Test
     public void measureEfficiencyLinkedList() {
         System.out.printf("LinkedList implementation%n");
-        int numberOfPlayers = 100;
+       
         LinkedList<Player> players = getSubList(unsortedPlayers, numberOfPlayers);
         AdvancedSorts.quickSort(players);
         LinkedList<Player> playersSorted = AdvancedSorts.quickSort(players);
@@ -38,5 +37,33 @@ public class ExtendedAdvancedSortingTest extends AdvancedSortingTest {
         // Try to keep measurements steady
         players.clear();
 
+    }
+    // Check if list is really sorted
+    @Test
+    public void checkIfSortingWorks(){
+        
+        LinkedList<Player> players = getSubList(unsortedPlayers, numberOfPlayers);
+        
+        LinkedList<Player> playersSorted = AdvancedSorts.quickSort(players);
+        boolean sorted = true;
+        for(int i = 0; i < playersSorted.size() - 1; i++){
+            if(playersSorted.get(i).compareTo(players.get(i + 1)) != 1){
+               sorted = false;
+               break;
+            }
+        }
+        Assert.assertEquals(true, sorted);
+  
+        
+    }
+    // Checks if size doesn't change on sort.
+    @Test
+    public void checkIfEven(){
+        LinkedList<Player> players = getSubList(unsortedPlayers, numberOfPlayers);
+        int initialSize = players.size();
+        
+        LinkedList<Player> playersSorted = AdvancedSorts.quickSort(players);
+        int sortedSize = playersSorted.size();
+        Assert.assertEquals(true, initialSize == sortedSize);
     }
 }
