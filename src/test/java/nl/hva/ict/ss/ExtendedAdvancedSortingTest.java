1b541d1b56dc22366b281d03479f2f771a36d345
Rowan Visser <rowan.visser@hotmail.com>
2018-11-30 12:30:19 +0100

    Added tests

--- src/test/java/nl/hva/ict/ss/ExtendedAdvancedSortingTest.java
+++ src/test/java/nl/hva/ict/ss/ExtendedAdvancedSortingTest.java
@@ -13,21 +13,17 @@ public class ExtendedAdvancedSortingTest extends AdvancedSortingTest {
 int numberOfPlayers = 100;
     @Test
     public void measureEfficiencyArray() {
-        System.out.printf("Array implementation V2%n");
-       
+        System.out.printf("Array implementation V2, Extended Advanced Sorting Test%n");
         Player[] players = getSubList(unsortedPlayers, numberOfPlayers).toArray(new Player[numberOfPlayers]);
-
         Player[] playersSorted = AdvancedSorts.quickSort(players);
         for (int i = 0; i < playersSorted.length; i++) {
             System.out.println(playersSorted[i].getHighScore());
         }
-
     }
 
     @Test
     public void measureEfficiencyLinkedList() {
-        System.out.printf("LinkedList implementation%n");
-       
+        System.out.printf("LinkedList implementation, Extended Advanced Sorting Test%n"); 
         LinkedList<Player> players = getSubList(unsortedPlayers, numberOfPlayers);
         AdvancedSorts.quickSort(players);
         LinkedList<Player> playersSorted = AdvancedSorts.quickSort(players);
@@ -36,14 +32,13 @@ int numberOfPlayers = 100;
         }
         // Try to keep measurements steady
         players.clear();
-
     }
+    
     // Check if list is really sorted
     @Test
-    public void checkIfSortingWorks(){
-        
+    public void checkIfSortingWorksLinkedList(){
+        System.out.printf("checkIfSortingWorksLinkedList, Extended Advanced Sorting Test%n"); 
         LinkedList<Player> players = getSubList(unsortedPlayers, numberOfPlayers);
-        
         LinkedList<Player> playersSorted = AdvancedSorts.quickSort(players);
         boolean sorted = true;
         for(int i = 0; i < playersSorted.size() - 1; i++){
@@ -52,16 +47,41 @@ int numberOfPlayers = 100;
                break;
             }
         }
-        Assert.assertEquals(true, sorted);
-  
-        
+        Assert.assertEquals(true, sorted);     
     }
+    
+    @Test
+    public void checkIfSortingWorksArray(){
+        System.out.printf("checkIfSortingWorksArray, Extended Advanced Sorting Test%n"); 
+        Player[] players = getSubList(unsortedPlayers, numberOfPlayers).toArray(new Player[numberOfPlayers]);
+        Player[] playersSorted = AdvancedSorts.quickSort(players);
+        boolean sorted = true;
+        for(int i = 0; i < playersSorted.length - 1; i++){
+            if(playersSorted[i].compareTo(players[i+1]) != 1){
+               sorted = false;
+               break;
+            }
+        }
+        Assert.assertEquals(true, sorted);
+    }
+    
     // Checks if size doesn't change on sort.
     @Test
-    public void checkIfEven(){
+    public void checkIfEvenArray(){
+        System.out.printf("checkIfEvenArray, Extended Advanced Sorting Test%n"); 
+        Player[] players = getSubList(unsortedPlayers, numberOfPlayers).toArray(new Player[numberOfPlayers]);
+        int initialSize = players.length;
+        Player[] playersSorted = AdvancedSorts.quickSort(players);
+        int sortedSize = playersSorted.length;
+        Assert.assertEquals(true, initialSize == sortedSize);
+    }
+    
+    // Checks if size doesn't change on sort.
+    @Test
+    public void checkIfEvenLinkedList(){
+        System.out.printf("checkIfEvenLinkedList, Extended Advanced Sorting Test%n"); 
         LinkedList<Player> players = getSubList(unsortedPlayers, numberOfPlayers);
         int initialSize = players.size();
-        
         LinkedList<Player> playersSorted = AdvancedSorts.quickSort(players);
         int sortedSize = playersSorted.size();
         Assert.assertEquals(true, initialSize == sortedSize);
